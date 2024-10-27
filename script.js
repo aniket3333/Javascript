aa42357156eb6d9568ebfa5403862c11ab8dd216
Aniket <aniket.hiware@sdaemon.co.in>
2024-10-27 18:10:48 +0530

    Javascript programs

---
+++ script.js
@@ -0,0 +1,27 @@
+let d1 = [1,2,3,4,5,11,12,13]
+let d2=[6,7,8,9,10];
+let d3=[];
+let d4 = [11,14,3,5,4];
+console.log(d4);
+// for(let i=0;i<d1.length;i++)
+// {
+//     d3[i] = d1[i];
+// }
+// for(let j=0;j<d2.length;j++)
+// {
+// d3[d1.length+j] = d2[j]
+// }
+// for(let i=0;i<d4.length;i++)
+// {
+//  for(let j=0;j<d4.length;j++)
+//  {
+//      if(d4[j] > d4[j+1])
+//      {
+//          let temp = d4[j];
+//          d4[j] = d4[j+1];
+//          d4[j+1]  =temp;
+//      }
+//  }
+// }
+// d3 = [...d1,...d2]
+console.log(d4);
